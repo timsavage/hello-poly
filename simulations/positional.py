dd341adceb3f0c378a7bdd4973f0e61efaf733c9
Tim Savage <tim@savage.company>
2019-03-21 23:01:47 +1100

    Blacked

--- simulations/positional.py
+++ simulations/positional.py
@@ -1,22 +1,20 @@
 NULL_NOTE = 255
 
 key_events = (
-    (True,   1),
-    (True,  12),
-    (True,   5),
-    (True,   8),
-
-    (False,  5),
-    (False,  8),
-    (True,   5),
+    (True, 1),
+    (True, 12),
+    (True, 5),
+    (True, 8),
+    (False, 5),
+    (False, 8),
+    (True, 5),
     (False, 12),
-    (False,  1),
-    (False,  5),
-
-    (True,   1),
-    (True,   5),
-    (True,   8),
-    (True,  12),
+    (False, 1),
+    (False, 5),
+    (True, 1),
+    (True, 5),
+    (True, 8),
+    (True, 12),
 )
 
 
@@ -25,11 +23,11 @@ class Position:
         self.count = count
         self.notes = [NULL_NOTE] * (count + 1)
         self.keys = [NULL_NOTE] * count
-    
+
     def __str__(self):
-        notes = ', '.join(f'{n:03}' for n in self.notes)
-        keys = ', '.join(f'{n:03}' for n in self.keys)
-        return f'[{notes}]{{{keys}}}]'
+        notes = ", ".join(f"{n:03}" for n in self.notes)
+        keys = ", ".join(f"{n:03}" for n in self.keys)
+        return f"[{notes}]{{{keys}}}]"
 
 
 class PositionLow(Position):
@@ -66,7 +64,7 @@ class PositionHigh(Position):
     def note_on(self, note):
         for idx in reversed(range(self.count)):
             val = self.notes[idx]
-            if note > val or val == NULL_NOTE :
+            if note > val or val == NULL_NOTE:
                 self.notes[idx + 1] = self.notes[idx]
                 self.notes[idx] = note
 
@@ -98,10 +96,10 @@ for press, note in key_events:
     if press:
         lo_model.note_on(note)
         hi_model.note_on(note)
-        action = '  Press'
+        action = "  Press"
     else:
         lo_model.note_off(note)
         hi_model.note_off(note)
-        action = 'Release'
+        action = "Release"
 
     print(f"{action} [{note:02}] - Lo: {lo_model}; Hi: {hi_model}")
