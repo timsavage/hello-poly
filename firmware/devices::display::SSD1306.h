045e725248035f140ff1d1ac0ec134ccf3778d78
Tim Savage <tim@savage.company>
2019-03-27 01:00:24 +1100

    Rebuilt display driver from modified Adafruit library

--- firmware/devices::display::SSD1306.h
+++ firmware/devices::display::SSD1306.h
@@ -1,27 +1,58 @@
-/**
- * 
- */
+/*********************************************************************
+This is a library for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+These displays use SPI to communicate, 4 or 5 pins are required to
+interface
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
 #pragma once
+
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
 
 namespace devices {
 namespace display {
-
-#include <Arduino.h>
 
 #define BLACK 0
 #define WHITE 1
 #define INVERSE 2
 
-#define SSD1306_I2C_ADDRESS   0x3C
+#define SSD1306_I2C_ADDRESS   0x3C  // 011110+SA0+RW - 0x3C or 0x3D
+// Address for 128x32 is 0x3C
+// Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
 
+/*=========================================================================
+    SSD1306 Displays
+    -----------------------------------------------------------------------
+    The driver is used in multiple displays (128x64, 128x32, etc.).
+    Select the appropriate display below to create an appropriately
+    sized framebuffer, etc.
+
+    SSD1306_128_64  128x64 pixel display
+
+    SSD1306_128_32  128x32 pixel display
+
+    SSD1306_96_16
+
+    -----------------------------------------------------------------------*/
 //   #define SSD1306_128_64
-#define SSD1306_128_32
-//   #define SSD1306_96_16
+   #define SSD1306_128_32
+/*=========================================================================*/
 
 #if defined SSD1306_128_64 && defined SSD1306_128_32
   #error "Only one SSD1306 display can be specified at once in SSD1306.h"
 #endif
-#if !defined SSD1306_128_64 && !defined SSD1306_128_32 && !defined SSD1306_96_16
+#if !defined SSD1306_128_64 && !defined SSD1306_128_32
   #error "At least one SSD1306 display must be specified in SSD1306.h"
 #endif
 
@@ -32,10 +63,6 @@ namespace display {
 #if defined SSD1306_128_32
   #define SSD1306_LCDWIDTH                  128
   #define SSD1306_LCDHEIGHT                 32
-#endif
-#if defined SSD1306_96_16
-  #define SSD1306_LCDWIDTH                  96
-  #define SSD1306_LCDHEIGHT                 16
 #endif
 
 #define SSD1306_SETCONTRAST 0x81
@@ -73,38 +100,15 @@ namespace display {
 #define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
 #define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
 
-class SSD1306 {
-public:
-	SSD1306();
 
-	void 
+class SSD1306 : public Adafruit_GFX 
+{
+public:
+	SSD1306(uint8_t switchvcc = SSD1306_SWITCHCAPVCC, uint8_t i2caddr = SSD1306_I2C_ADDRESS);
+
+  	void 
 	begin();
 
-	void 
-	clearDisplay(void);
-	
-	void 
-	invertDisplay(uint8_t i);
-	
-	void 
-	display();
-
-//   void startscrollright(uint8_t start, uint8_t stop);
-//   void startscrollleft(uint8_t start, uint8_t stop);
-
-//   void startscrolldiagright(uint8_t start, uint8_t stop);
-//   void startscrolldiagleft(uint8_t start, uint8_t stop);
-//   void stopscroll(void);
-
-  	void 
-	dim(boolean dim);
-
-  	void 
-	drawPixel(int16_t x, int16_t y, uint16_t color);
-
-//   virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-//   virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-protected:
 	void 
 	sendCommand(uint8_t c);
 	void 
@@ -112,13 +116,44 @@ protected:
 	void 
 	sendCommand(uint8_t c, uint8_t a1, uint8_t a2);
 
+	void 
+	display();
+
+	void 
+	clearDisplay(void);
+	void 
+	invertDisplay(uint8_t i);
+
+	void 
+	startscrollright(uint8_t start, uint8_t stop);
+	void 
+	startscrollleft(uint8_t start, uint8_t stop);
+	void 
+	startscrolldiagright(uint8_t start, uint8_t stop);
+	void 
+	startscrolldiagleft(uint8_t start, uint8_t stop);
+	void 
+	stopscroll(void);
+
+	void 
+	dim(boolean dim);
+
+	void 
+	drawPixel(int16_t x, int16_t y, uint16_t color);
+
+	virtual void 
+	drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+	virtual void 
+	drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+
 private:
-  	int8_t _i2caddr;
+	int8_t _i2caddr;
 	int8_t _vccstate;
 
-//   inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
-//   inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
-
+	inline void 
+	drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
+	inline void 
+	drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
 };
 
 }}  //! display::devices
